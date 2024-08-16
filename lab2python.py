170d8426de78e342ec2cb57eb81d295efbdfc1b0
lewisemma <123219477+lewisemma@users.noreply.github.com>
2024-08-16 15:04:02 -0400

    Update lab2python.py

--- lab2python.py
+++ lab2python.py
@@ -5,4 +5,26 @@
 #In this lab we will mainly convert those outcomes into Training Labels with 1 means the booster successfully landed 0 means it was unsuccessful.
 #Falcon 9 first stage will land successfully
 
+#Objectives
+#Perform exploratory Data Analysis and determine Training Labels
+
+#Exploratory Data Analysis
+#Determine Training Labels
+
+# Pandas is a software library written for the Python programming language for data manipulation and analysis.
+import pandas as pd
+#NumPy is a library for the Python programming language, adding support for large, multi-dimensional arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays
+import numpy as np
+
+df=pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/dataset_part_1.csv")
+df.head(10)
+
+df.isnull().sum()/len(df)*100
+#Identify and calculate the percentage of the missing values in each attribute
+
+df.dtypes
+#Identify which columns are numerical and categorical:
+
+
+
 
